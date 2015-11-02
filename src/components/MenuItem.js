38572d109e4eac8212f91c808a6e3d8a9dd7bd7d
Johannes Stein <johannesstein@freeze-dev.com>
2015-11-02 02:13:13 +0100

    Update item style

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -2,12 +2,16 @@ import React, {Component} from 'react';
 
 class MenuItem extends Component {
   render() {
+    const description = this.props.description.split('\n').map((line, index) => {
+      return <div className="line" key={index}>{line}</div>;
+    });
+
     return (
       <div className="menu-item" onClick={this.props.onClick}>
         <div className={`menu-item-icon menu-item-icon-${this.props.icon}`} />
         <div className="menu-item-text">
           <h3>{this.props.name}</h3>
-          <div className="menu-item-description">{this.props.description}</div>
+          <div className="menu-item-description">{description}</div>
         </div>
       </div>
     )
